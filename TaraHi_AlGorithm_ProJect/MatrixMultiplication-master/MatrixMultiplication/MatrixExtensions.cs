1f938cd52459b21d4923a6c89937282a424f6c84
overwatch11m99 <45873243+overwatch11m99@users.noreply.github.com>
2019-06-30 22:27:51 +0430

    strassen

---
+++ TaraHi_AlGorithm_ProJect/MatrixMultiplication-master/MatrixMultiplication/MatrixExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace MatrixMultiplication
+{
+    public static class MatrixExtensions
+    {
+        public static void Write(this Matrix matrix, TextWriter output, int elemSize = 3, bool showPlus = true)
+        {
+            for (int row = 0; row < matrix.Rows; row++)
+            {
+                for (int col = 0; col < matrix.Columns; col++)
+                {
+                    output.Write(matrix[row, col].ToString().PadRight(elemSize));
+                }
+                output.WriteLine();
+            }
+        }
+    }
+}
