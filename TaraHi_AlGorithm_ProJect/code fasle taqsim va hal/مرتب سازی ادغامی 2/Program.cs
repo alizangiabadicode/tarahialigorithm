2d4405afe791fe57a37f78baf3d07d852ec6cdd3
overwatch11m99 <45873243+overwatch11m99@users.noreply.github.com>
2019-06-30 22:24:05 +0430

    false yek

---
+++ TaraHi_AlGorithm_ProJect/code fasle taqsim va hal/مرتب سازی ادغامی 2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace مرتب_سازی_ادغامی_2
+{
+    class Program
+    {
+
+        static int[] s =null;
+        static void MergeSort2(int low , int high){
+            if(low < high){
+
+                int mid = (low + high)/2;
+
+                MergeSort2(low,mid);
+                MergeSort2(mid + 1,high);
+
+                Merge2(low,mid,high);
+            }
+        }
+
+        static void Merge2(int low , int mid, int high){
+            int i =low , j=mid+1 , k=0;
+            int[] arr = new int[high - low + 1];
+            while(i<=mid && j<=high){
+                if(s[i]<s[j]){
+                    arr[k]=s[i];
+                    i++;
+                }
+                else if(s[j]<s[i]){
+                    arr[k]=s[j];
+                    j++;
+                }
+                k++;
+            }
+            if(i>mid){
+                while (j<=high)
+                {
+                    arr[k]=s[j];
+                    j++;
+                    k++;
+                }
+            }
+            else if(j>high){
+                while (i<=mid){
+                    arr[k]=s[i];
+                    i++;
+                    k++;
+                }
+            }
+            int c=0;
+            for (int o = low; o <= high; o++)
+            {
+                s[o] = arr[c++];
+            }
+        }
+
+
+        static void Display(int[] arr){
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("{0} ",arr[i]);
+            }
+            Console.WriteLine();
+        }
+
+
+        static void Main(string[] args)
+        {
+            s=new int[]{
+                1,4,2,6,5,7
+            };
+            Display(s);
+            MergeSort2(0,s.Length-1);
+            Display(s);
+        }
+    }
+}
---
+++ TaraHi_AlGorithm_ProJect/code fasle taqsim va hal/مرتب سازی سریع/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace مرتب_سازی_سریع
+{
+    class Program
+    {
+
+        static int[] s = null;
+        static Random rnd = new Random();
+        static void QuickSort(int low, int high)
+        {
+            int pivotPoint =0;
+            if (low < high)
+            {
+                pivotPoint = Partition(low,high);
+                QuickSort(low, pivotPoint - 1);
+                QuickSort(pivotPoint + 1, high);
+            }
+        }
+        
+        static int Partition(int low,int high){
+            int pivotItem = s[low];
+            int j = low;
+            for (int i = low; i <= high; i++)
+            {
+                if(s[i] < pivotItem){
+                    j++;
+                    Exchange(i,j);
+                }
+            }
+            Exchange(low,j);
+            return j;
+        }
+
+        static void Exchange(int i, int j){
+            int temp = s[j];
+            s[j]=s[i];
+            s[i]=temp;
+        }
+
+        static void Display(){
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.Write("{0} ",s[i]);
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            s = new int[]{
+                2,7,5,1,4
+            };
+            Display();
+            QuickSort(0,s.Length -1);
+            Display();
+        }
+    }
+}
