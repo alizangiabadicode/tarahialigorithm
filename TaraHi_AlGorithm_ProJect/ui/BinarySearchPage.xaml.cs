b5a8080e05f25e6055b430013f191fc8f1cfc123
Mohammad hosein Alizadeh <m.h.alizadeh2017@outlook.com>
2019-07-07 19:50:01 +0430

    base ui

---
+++ TaraHi_AlGorithm_ProJect/ui/BinarySearchPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace TaraHi_AlGorithm_ProJect.ui
+{
+    /// <summary>
+    /// Interaction logic for BinarySearchPage.xaml
+    /// </summary>
+    public partial class BinarySearchPage : Page
+    {
+        public BinarySearchPage()
+        {
+            InitializeComponent();
+        }
+        int BinarySearch(int low, int high, int x, int[] s)
+        {
+            if (low > high)
+            {
+                return 0;
+            }
+            else
+            {
+                int mid = (low + high) / 2;
+                if (s[mid] == x)
+                    return mid;
+
+                else if (x < s[mid])
+                    BinarySearch(low, mid - 1, x, s);
+
+                else if (x > s[mid])
+                    BinarySearch(mid + 1, high, x, s);
+            }
+            return -1;
+        }
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
